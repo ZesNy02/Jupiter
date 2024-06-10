fbb9cac5a2a62198a3792a61419ae46fa9dd05e1
Lenny <lenny-zesewitz@web.de>
2024-06-10 10:52:56 +0200

    files zum testen von modularen Files mit Github als fileserver

---
+++ scrip1.js
@@ -0,0 +1,4 @@
+function greet1() {
+    window.alert("Hello from remote script 1");
+}
+window.greet1 = greet1; // Exportieren als globales Objekt
---
+++ script2.js
@@ -0,0 +1,4 @@
+function greet2() {
+    window.alert("Hello from remote script 2");
+}
+window.greet2 = greet2; // Exportieren als globales Objekt
