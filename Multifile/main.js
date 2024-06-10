6fa224875790d474c7849be569e4d4cf8202bba0
Lenny <lenny-zesewitz@web.de>
2024-06-10 12:52:12 +0200

    Test ob das file richtig geladen wird

--- Multifile/main.js
+++ Multifile/main.js
@@ -1,6 +1,7 @@
 import makeChatButton from './button.js'; // Importiere die JavaScript-Funktion
 import './style.css'; // Importiere die CSS-Datei
 
+console.log("main.js"); // Gebe eine Nachricht in der Konsole aus
 // Erstelle den Chat-Button und füge ihn zur Seite hinzu
 document.body.appendChild(makeChatButton());
 window.alert("Test"); // Teste, ob die JavaScript-Datei korrekt geladen wurde
