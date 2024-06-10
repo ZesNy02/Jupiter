76e66e7347da75a174a57ebf80b2f05ae3dceffb
Lenny <lenny-zesewitz@web.de>
2024-06-10 13:24:06 +0200

    Test ob das file richtig geladen wird

---
+++ Multifile/button.mjs
@@ -0,0 +1,16 @@
+// Erstelle einen Button 
+const makeChatButton = () => {
+    const chatButton = document.createElement("button");
+    chatButton.id = "chat-button";
+    chatButton.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" height="90%" viewBox="0 -960 960 960" width="90%"><path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/></svg>';
+    chatButton.classList.add("chat-button"); // Hinzufügen einer CSS-Klasse
+    chatButton.addEventListener("click", () => {
+        const chatWindow = document.getElementById("chat-window");
+        chatWindow.style.display =
+            chatWindow.style.display === "none" ? "flex" : "none";
+    });
+    return chatButton;
+};
+
+export default makeChatButton;
