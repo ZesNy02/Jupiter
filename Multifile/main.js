99adee67eea8af4ac666b50468f47d704eaa4e9d
Lenny <lenny.zesewitz@gmail.com>
2024-06-11 11:48:14 +0200

    funktioniert es ohne imports?

--- Multifile/main.js
+++ Multifile/main.js
@@ -1,7 +1,4 @@
-import {makeChatButton} from './button.mjs'; // Importiere die JavaScript-Funktion
-import './style.css'; // Importiere die CSS-Datei
-
 console.log("main.js"); // Gebe eine Nachricht in der Konsole aus
 // Erstelle den Chat-Button und füge ihn zur Seite hinzu
-document.body.appendChild(makeChatButton());
+// document.body.appendChild(makeChatButton());
 window.alert("Test"); // Teste, ob die JavaScript-Datei korrekt geladen wurde
