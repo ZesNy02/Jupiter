f836b0a7322496f377914dbed2e7489acbeebd10
3002217 <3002217@stud.hs-mannheim.de>
2024-06-18 13:30:08 +0200

    größe ist nun gleich wie die vorhandenen symbole

--- Tampermonkey/script.js
+++ Tampermonkey/script.js
@@ -262,10 +262,10 @@ const makeChatButton = () => {
     chatButton.innerHTML =
         '<svg class="chatButtonSVG" id="chatButtonSVG" xmlns="http://www.w3.org/2000/svg" height="90%" viewBox="0 -960 960 960" width="90%" fill="#D9D9D9"><path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/></svg>';
     chatButton.style.position = "fixed";
-    chatButton.style.bottom = "10px";
+    chatButton.style.bottom = "20px";
     chatButton.style.left = "270px";
-    chatButton.style.width = "48px";
-    chatButton.style.height = "48px";
+    chatButton.style.width = "3.3rem";
+    chatButton.style.height = "3.3rem";
     chatButton.style.borderRadius = "100%";
     chatButton.style.border = "none";
     chatButton.style.zIndex = "80";
@@ -504,7 +504,7 @@ const makeChatWindow = () => {
     chatWindow.id = "chatWindow";
     chatWindow.className = "chatWindow";
     chatWindow.style.position = "fixed";
-    chatWindow.style.bottom = "10px";
+    chatWindow.style.bottom = "1.42rem";
     chatWindow.style.left = "270px";
     chatWindow.style.width = "30vw";
     chatWindow.style.height = "50vh";
