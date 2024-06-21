8095d1d9b6c6667db2e8f8820baf8ea3c8c01de3
Tonka-de <rmcmaster05@gmail.com>
2024-06-21 15:42:30 +0200

    add prompts, fixes, auto scroll

--- chaty/src/components/ChatInput/ChatInput.tsx
+++ chaty/src/components/ChatInput/ChatInput.tsx
@@ -1,11 +1,29 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./ChatInput.css";
 
 interface ChatInputProps {
   eventStorming?: boolean;
+  onSend: (promt: string) => void;
 }
 
-const ChatInput: FC<ChatInputProps> = ({ eventStorming }) => {
+const ChatInput: FC<ChatInputProps> = ({ eventStorming, onSend }) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const handlePressEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSend();
+    }
+  };
+
+  const handleSend = () => {
+    onSend(inputText);
+    setInputText("");
+  };
+
   return (
     <>
       <div className="chatInputWrapper">
@@ -17,8 +35,11 @@ const ChatInput: FC<ChatInputProps> = ({ eventStorming }) => {
               ? "Write your topic here..."
               : "Write your question here..."
           }
+          value={inputText}
+          onKeyUp={handlePressEnter}
+          onChange={handleTextChange}
         />
-        <button className="chatInputSendButton">
+        <button className="chatInputSendButton" onClick={handleSend}>
           <img src="/ChatInputIcon.svg" className="chatInputSendButtonIcon" />
         </button>
       </div>
