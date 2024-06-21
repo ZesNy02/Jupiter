c417a318e8f53d16547502d0ae93f42f87c1dc87
Tonka-de <rmcmaster05@gmail.com>
2024-06-21 18:45:51 +0200

    not much

--- chaty/src/components/MainChatWindow/MainChatWindow.tsx
+++ chaty/src/components/MainChatWindow/MainChatWindow.tsx
@@ -4,7 +4,7 @@ import ChatHeader from "../ChatHeader";
 import ChatWrapper from "../ChatWrapper";
 import ChatInput from "../ChatInput";
 import ESToggleButton from "../ESToggleButton";
-import { Prompt } from "../ChatWrapper/ChatWrapper";
+import { Answer, Prompt } from "../ChatMessage/ChatMessage";
 
 interface MainChatWindowProps {
   onClose: () => void;
@@ -18,35 +18,26 @@ const example: Prompt[] = [
       {
         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         answer: true,
-        error: false,
-        eventStorming: false,
         count: 1,
         maxCount: 4,
-        loading: false,
       },
       {
         message: "Error message",
         answer: true,
         error: true,
-        eventStorming: false,
         count: 2,
         maxCount: 4,
-        loading: false,
       },
       {
         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         answer: true,
-        error: false,
         eventStorming: true,
         count: 3,
         maxCount: 4,
-        loading: false,
       },
       {
         message: "",
         answer: true,
-        error: false,
-        eventStorming: false,
         count: 4,
         maxCount: 4,
         loading: true,
@@ -55,26 +46,63 @@ const example: Prompt[] = [
   },
 ];
 
+type ServerResponse = { Success: "ok" } | { Error: string };
+
 const MainChatWindow: FC<MainChatWindowProps> = ({ onClose }) => {
   const [eventStormingState, setEventStormingState] = useState(false);
   const [prompts, setPrompts] = useState<Prompt[]>(example);
+
+  const addPrompt = (prompt: string, promptId: number) => {
+    setPrompts((prev) => {
+      return [
+        ...prev,
+        {
+          message: prompt,
+          id: promptId,
+          answers: [],
+        },
+      ];
+    });
+  };
+
+  const addAnswer = (promptId: number, answer: Answer) => {
+    setPrompts((prev) => {
+      return prev.map((prompt) => {
+        if (prompt.id === promptId) {
+          return {
+            ...prompt,
+            answers: [...prompt.answers, answer],
+          };
+        }
+        return prompt;
+      });
+    });
+  };
 
   const handleToggleEventStorming = () => {
     setEventStormingState(!eventStormingState);
   };
 
   const handlePromptRequest = (prompt: string) => {
-    // TODO: implement
-    setPrompts((prev) => {
-      return [
-        ...prev,
-        {
-          message: prompt,
-          id: prev.length + 1,
-          answers: [],
-        },
-      ];
-    });
+    addPrompt(prompt, 1);
+  };
+
+  const handleRegenerateRequest = (promptId: number) => {
+    const prompt = prompts.find((p) => p.id === promptId);
+    if (!prompt) {
+      return;
+    }
+    addAnswer(promptId, { message: "", answer: true, loading: true });
+  };
+
+  const handleRatingRequest = (
+    promptId: number,
+    answerIndex: number,
+    rating: number
+  ) => {};
+
+  const handleEventStormingRequest = (prompt: string) => {
+    addPrompt(prompt, 1);
   };
 
   return (
