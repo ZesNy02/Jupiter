e475ece846e02097c7abbe16936a7e88659a8a5e
Lenny <lenny-zesewitz@web.de>
2024-06-21 11:04:49 +0200

    MainCHatWindow.tsx erstellen und mit erster logik befüllen

--- chaty/src/components/MainChatWindow/MainChatWindow.tsx
+++ chaty/src/components/MainChatWindow/MainChatWindow.tsx
@@ -1,10 +1,21 @@
 import { FC } from "react";
 import "./MainChatWindow.css";
+import ChatHeader from "../ChatHeader";
+import ChatWrapper from "../ChatWrapper";
+import ChatInput from "../ChatInput";
+import ESToggleButton from "../ESToggleButton";
 
 interface MainChatWindowProps {}
 
-const MainChatWindow: FC<MainChatWindowProps> = ({}) => {
-  return <>Component</>;
-};
+const MainChatWindow: FC<MainChatWindowProps> = () => {
+
+    return(
+      <div>
+        <ChatHeader />
+        <ChatWrapper />
+        <ESToggleButton />
+        <ChatInput />
+      </div>
+)};
 
 export default MainChatWindow;
