fdabed06f0e9b5d8a776b469e9ff85e358305ad8
Tonka-de <rmcmaster05@gmail.com>
2024-06-21 12:23:15 +0200

    final html

--- chaty/src/components/ChatInput/ChatInput.tsx
+++ chaty/src/components/ChatInput/ChatInput.tsx
@@ -1,10 +1,29 @@
 import { FC } from "react";
 import "./ChatInput.css";
 
-interface ChatInputProps {}
+interface ChatInputProps {
+  eventStorming?: boolean;
+}
 
-const ChatInput: FC<ChatInputProps> = ({}) => {
-  return <>Component</>;
+const ChatInput: FC<ChatInputProps> = ({ eventStorming }) => {
+  return (
+    <>
+      <div className="chatInputWrapper">
+        <input
+          type="text"
+          className="chatInput"
+          placeholder={
+            eventStorming
+              ? "Write your topic here..."
+              : "Write your question here..."
+          }
+        />
+        <button className="chatInputSendButton">
+          <img src="/ChatInputIcon.svg" className="chatInputSendButtonIcon" />
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default ChatInput;
--- chaty/src/components/ChatWrapper/ChatWrapper.tsx
+++ chaty/src/components/ChatWrapper/ChatWrapper.tsx
@@ -1,10 +1,26 @@
 import { FC } from "react";
 import "./ChatWrapper.css";
+import ChatMessage from "../ChatMessage";
 
 interface ChatWrapperProps {}
 
 const ChatWrapper: FC<ChatWrapperProps> = ({}) => {
-  return <>Component</>;
+  const list = ["message", "message", "message"];
+  return (
+    <>
+      <div className="chatWrapper">
+        {list.map((message, index) => (
+          <ChatMessage
+            key={index}
+            message={message}
+            answer={index % 2 === 0}
+            count={index}
+            maxCount={list.length}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default ChatWrapper;
