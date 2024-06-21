852021a48e2ca199473e904b4a93aedc6ced0ba7
CedricNw <hermannce@web.de>
2024-06-21 17:22:39 +0200

    start page added to main

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar/Navbar.tsx";
 import Footer from "./Components/Footer/Footer.tsx";
+import Start from "./pages/Start.tsx";
 
 const App = () => {
 
   return (
       <div className="App">
         <Navbar />
+          <Start/>
         <Footer />
       </div>
   );
