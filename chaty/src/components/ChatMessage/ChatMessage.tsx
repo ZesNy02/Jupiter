b7fa7f89da8706d486ad4d099f420578670316c4
Tonka-de <rmcmaster05@gmail.com>
2024-06-21 11:12:27 +0200

    Chat Message und ES Toggle html

--- chaty/src/components/ChatMessage/ChatMessage.tsx
+++ chaty/src/components/ChatMessage/ChatMessage.tsx
@@ -1,10 +1,49 @@
 import { FC } from "react";
 import "./ChatMessage.css";
 
-interface ChatMessageProps {}
+interface ChatMessageProps {
+  message: string;
+  answer: boolean;
+  count?: number;
+  maxCount?: number;
+}
 
-const ChatMessage: FC<ChatMessageProps> = ({}) => {
-  return <>Component</>;
+const ChatMessage: FC<ChatMessageProps> = ({
+  message,
+  answer,
+  count,
+  maxCount,
+}) => {
+  return (
+    <>
+      <div
+        className={`chatMessageWrapper ${
+          answer ? "chatyMessage" : "userMessage"
+        }`}
+      >
+        <div className="chatMessage">
+          <p className="chatMessageContent">{message}</p>
+          {answer && <p>{`${count}/${maxCount}`}</p>}
+        </div>
+        {answer && (
+          <div className="chatMessageActions">
+            <button className="chatMessageActionButton">
+              <img src="/ReloadIcon.svg" className="chatMessageActionIcon" />
+            </button>
+            <button className="chatMessageActionButton">
+              <img src="/ThumbsUpIcon.svg" className="chatMessageActionIcon" />
+            </button>
+            <button className="chatMessageActionButton">
+              <img
+                src="/ThumbsDownIcon.svg"
+                className="chatMessageActionIcon"
+              />
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ChatMessage;
--- chaty/src/components/ESToggleButton/ESToggleButton.tsx
+++ chaty/src/components/ESToggleButton/ESToggleButton.tsx
@@ -1,10 +1,23 @@
 import { FC } from "react";
 import "./ESToggleButton.css";
 
-interface ESToggleButtonProps {}
+interface ESToggleButtonProps {
+  onClick: () => void;
+}
 
-const ESToggleButton: FC<ESToggleButtonProps> = ({}) => {
-  return <>Component</>;
+const ESToggleButton: FC<ESToggleButtonProps> = ({ onClick }) => {
+  return (
+    <>
+      <div className="eventstormingWrapper">
+        <button className="toggleButtonWrapper" onClick={onClick}>
+          <div className="toggleButtonBackground">
+            <span className="toggleButtonPoint"></span>
+          </div>
+        </button>
+        <h2 className="eventstormingTitle"></h2>
+      </div>
+    </>
+  );
 };
 
 export default ESToggleButton;
