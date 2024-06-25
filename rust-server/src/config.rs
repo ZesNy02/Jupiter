cee6ec358404a48b8013b651f958e1dda604c179
Vincent <3003750@stud.hs-mannheim.de>
2024-06-25 14:08:47 +0200

    rust angepasst an python

--- rust-server/src/config.rs
+++ rust-server/src/config.rs
@@ -7,119 +7,119 @@ use crate::models::database::DBConnectionInfo;
 /// Represents the configuration settings.
 #[derive(PartialEq, Debug, Clone)]
 pub struct Config {
-  /// The IP address of the server.
-  pub ip: String,
-  /// The port of the server.
-  pub port: u16,
-  /// The connection information for the PostgreSQL database.
-  pub db_connection: DBConnectionInfo,
-  /// The connection string for the LLM Server (Ollama).
-  pub llm_connection: String,
-  /// The path to the Python RAG script file.
-  pub rag_script: String,
-  /// The path to the Python vector query script file.
-  pub vector_query_script: String,
-  /// The path to the Python event storming script file.
-  pub event_storming_script: String,
+    /// The IP address of the server.
+    pub ip: String,
+    /// The port of the server.
+    pub port: u16,
+    /// The connection information for the PostgreSQL database.
+    pub db_connection: DBConnectionInfo,
+    /// The connection string for the LLM Server (Ollama).
+    pub llm_connection: String,
+    /// The path to the Python RAG script file.
+    pub rag_script: String,
+    /// The path to the Python vector query script file.
+    pub vector_query_script: String,
+    /// The path to the Python event storming script file.
+    pub event_storming_script: String,
 }
 
 impl Config {
-  /// Loads the configuration from environment variables.
-  ///
-  /// # Returns
-  ///
-  /// The loaded configuration.
-  ///
-  /// # Environment Variables
-  ///
-  /// The following environment variables are used to load the configuration,
-  /// and the ones in **bold** are required, if they are not found
-  /// the function will panic:
-  /// - **`DB_HOST`**: The host/ip of the PostgreSQL database.
-  /// - **`DB_NAME`**: The name of the PostgreSQL database.
-  /// - **`DB_USER`**: The username for the PostgreSQL database.
-  /// - **`DB_PASSWORD`**: The password for the PostgreSQL database.
-  /// - **`LLM_SERVER`**: The connection url for the LLM Server (Ollama).
-  /// - `IP`: The IP address of the server. Defaults to `127.0.0.1`.
-  /// - `PORT`: The port of the server. Defaults to `3000`.
-  /// - `DB_PORT`The port of the PostgreSQL database. Defaults to `5432`.
-  ///
-  /// # Panics
-  ///
-  /// - Panics if the [DBConnectionInfo] cannot be loaded
-  /// from the environment variables.
-  /// - Panics if the LLM Server connection cannot be loaded
-  /// from the environment variables.
-  pub fn load_from_env() -> Config {
-    dotenv().ok();
-    let db_host = env::var("DB_HOST").expect("Couldn't find DB_HOST in environment variables");
-    let db_name = env::var("DB_NAME").expect("Couldn't find DB_NAME in environment variables");
-    let db_user = env::var("DB_USER").expect("Couldn't find DB_USER in environment variables");
-    let db_password = env
-      ::var("DB_PASSWORD")
-      .expect("Couldn't find DB_PASSWORD in environment variables");
+    /// Loads the configuration from environment variables.
+    ///
+    /// # Returns
+    ///
+    /// The loaded configuration.
+    ///
+    /// # Environment Variables
+    ///
+    /// The following environment variables are used to load the configuration,
+    /// and the ones in **bold** are required, if they are not found
+    /// the function will panic:
+    /// - **`DB_HOST`**: The host/ip of the PostgreSQL database.
+    /// - **`DB_NAME`**: The name of the PostgreSQL database.
+    /// - **`DB_USER`**: The username for the PostgreSQL database.
+    /// - **`DB_PASSWORD`**: The password for the PostgreSQL database.
+    /// - **`LLM_SERVER`**: The connection url for the LLM Server (Ollama).
+    /// - `IP`: The IP address of the server. Defaults to `127.0.0.1`.
+    /// - `PORT`: The port of the server. Defaults to `3000`.
+    /// - `DB_PORT`The port of the PostgreSQL database. Defaults to `5432`.
+    ///
+    /// # Panics
+    ///
+    /// - Panics if the [DBConnectionInfo] cannot be loaded
+    /// from the environment variables.
+    /// - Panics if the LLM Server connection cannot be loaded
+    /// from the environment variables.
+    pub fn load_from_env() -> Config {
+        dotenv().ok();
+        let db_host = env::var("DB_HOST").expect("Couldn't find DB_HOST in environment variables");
+        let db_name = env::var("DB_NAME").expect("Couldn't find DB_NAME in environment variables");
+        let db_user = env::var("DB_USER").expect("Couldn't find DB_USER in environment variables");
+        let db_password = env
+            ::var("DB_PASSWORD")
+            .expect("Couldn't find DB_PASSWORD in environment variables");
 
-    let llm_server = env
-      ::var("LLM_SERVER")
-      .expect("Couldn't find LLM_SERVER in environment variables");
+        let llm_server = env
+            ::var("LLM_SERVER")
+            .expect("Couldn't find LLM_SERVER in environment variables");
 
-    // load IP of the Server from the environment variables or default to 127.0.0.1
-    let ip = env
-      ::var("IP")
-      .ok()
-      .unwrap_or_else(|| {
-        info!("IP not found in .env file. Using default IP 127.0.0.1.");
-        return "127.0.0.1".to_string();
-      });
+        // load IP of the Server from the environment variables or default to 127.0.0.1
+        let ip = env
+            ::var("IP")
+            .ok()
+            .unwrap_or_else(|| {
+                info!("IP not found in .env file. Using default IP 127.0.0.1.");
+                return "127.0.0.1".to_string();
+            });
 
-    // load Port of the Server from the environment variables or default to 3000
-    let port = env
-      ::var("PORT")
-      .ok()
-      .unwrap_or_else(|| {
-        info!("Port not found in .env file. Using default port 3000.");
-        return "3000".to_string();
-      })
-      .parse::<u16>()
-      .unwrap_or_else(|_port| {
-        error!("Failed to parse port. Using default port 3000.");
-        return 3000;
-      });
+        // load Port of the Server from the environment variables or default to 3000
+        let port = env
+            ::var("PORT")
+            .ok()
+            .unwrap_or_else(|| {
+                info!("Port not found in .env file. Using default port 3000.");
+                return "3000".to_string();
+            })
+            .parse::<u16>()
+            .unwrap_or_else(|_port| {
+                error!("Failed to parse port. Using default port 3000.");
+                return 3000;
+            });
 
-    // load Port of the Database from the environment variables or default to 5432
-    let db_port = env
-      ::var("DB_PORT")
-      .ok()
-      .unwrap_or_else(|| {
-        info!("DB_PORT not found in .env file. Using default port 5432.");
-        return "5432".to_string();
-      })
-      .parse::<u16>()
-      .unwrap_or_else(|_port| {
-        error!("Failed to parse DB_PORT. Using default port 5432.");
-        return 5432;
-      });
+        // load Port of the Database from the environment variables or default to 5432
+        let db_port = env
+            ::var("DB_PORT")
+            .ok()
+            .unwrap_or_else(|| {
+                info!("DB_PORT not found in .env file. Using default port 5432.");
+                return "5432".to_string();
+            })
+            .parse::<u16>()
+            .unwrap_or_else(|_port| {
+                error!("Failed to parse DB_PORT. Using default port 5432.");
+                return 5432;
+            });
 
-    Config {
-      ip,
-      port,
-      db_connection: DBConnectionInfo::new(db_host, db_port, db_name, db_user, db_password),
-      llm_connection: llm_server,
-      rag_script: "./scripts/python/ai/run_rag.py".to_string(),
-      vector_query_script: "./scripts/python/ai/vector_query.py".to_string(),
-      event_storming_script: "./scripts/python/ai/storming.py".to_string(),
+        Config {
+            ip,
+            port,
+            db_connection: DBConnectionInfo::new(db_host, db_port, db_name, db_user, db_password),
+            llm_connection: llm_server,
+            rag_script: "./scripts/python/execute_rag_query.py".to_string(),
+            vector_query_script: "./scripts/python/execute_vector_query.py".to_string(),
+            event_storming_script: "./scripts/python/execute_event_storming_query.py".to_string(),
+        }
     }
-  }
 
-  /// Gets the IP address and port of the server formatted as `IP:PORT`.
-  pub fn get_ported_ip(&self) -> String {
-    return format!("{}:{}", self.ip, self.port);
-  }
+    /// Gets the IP address and port of the server formatted as `IP:PORT`.
+    pub fn get_ported_ip(&self) -> String {
+        return format!("{}:{}", self.ip, self.port);
+    }
 
-  /// Gets the connection string for the database.
-  ///
-  /// Formatted as `host=HOST port=PORT dbname=DB_NAME user=DB_USER password=DB_PASSWORD`.
-  pub fn get_db_connection_string(&self) -> String {
-    return self.db_connection.to_string();
-  }
+    /// Gets the connection string for the database.
+    ///
+    /// Formatted as `host=HOST port=PORT dbname=DB_NAME user=DB_USER password=DB_PASSWORD`.
+    pub fn get_db_connection_string(&self) -> String {
+        return self.db_connection.to_string();
+    }
 }
--- rust-server/src/utils/python.rs
+++ rust-server/src/utils/python.rs
@@ -1,6 +1,6 @@
 use crate::{
-  config::Config,
-  models::{ python_ai::{ PythonError, Result }, routes_data::VectorSearchResult },
+    config::Config,
+    models::{ python_ai::{ PythonError, Result }, routes_data::VectorSearchResult },
 };
 use std::{ path::Path, process::Command };
 
@@ -25,33 +25,35 @@ use super::postgres::get_connection;
 /// * [PythonError::ResponseError] if the script returns an empty response.
 /// * [PythonError::IOError] if there is an IO error.
 pub fn run_script(script_path: String, args: Vec<String>) -> Result<String> {
-  if Path::new(&script_path).exists() == false {
-    return Err(PythonError::PathError(format!("Path does not exist: {}", script_path)));
-  }
-
-  // run the python script and wait for the output
-  let response = Command::new("python3").arg(script_path).args(args).output();
-
-  // match the response from the script
-  match response {
-    Ok(response) => {
-      // convert the response to a string
-      let response = String::from_utf8_lossy(&response.stdout).to_string();
-
-      if response.starts_with("Error: ") {
-        return Err(PythonError::ScriptError(response));
-      }
-
-      if response.is_empty() {
-        return Err(PythonError::ResponseError("Script returned an empty response".to_string()));
-      }
-
-      return Ok(response.to_string());
+    if Path::new(&script_path).exists() == false {
+        return Err(PythonError::PathError(format!("Path does not exist: {}", script_path)));
     }
-    Err(_e) => {
-      return Err(PythonError::IOError("Error while reading the python script".to_string()));
+
+    // run the python script and wait for the output
+    let response = Command::new("python3").arg(script_path).args(args).output();
+
+    // match the response from the script
+    match response {
+        Ok(response) => {
+            // convert the response to a string
+            let response = String::from_utf8_lossy(&response.stdout).to_string();
+
+            if response.starts_with("Error: ") {
+                return Err(PythonError::ScriptError(response));
+            }
+
+            if response.is_empty() {
+                return Err(
+                    PythonError::ResponseError("Script returned an empty response".to_string())
+                );
+            }
+
+            return Ok(response.to_string());
+        }
+        Err(_e) => {
+            return Err(PythonError::IOError("Error while reading the python script".to_string()));
+        }
     }
-  }
 }
 
 /// Handles the vector search request.
@@ -84,40 +86,39 @@ pub fn run_script(script_path: String, args: Vec<String>) -> Result<String> {
 /// - An error if the response can't be matched as `error` in the format
 /// `Couldn't match response: <script_result>`.
 pub async fn handle_vector_search(config: &Config, prompt: &String) -> Result<VectorSearchResult> {
-  let script_path = config.vector_query_script.clone();
-  let db_url = config.db_connection.to_string();
-  let prompt = prompt.clone();
+    let script_path = config.vector_query_script.clone();
+    let prompt = prompt.clone();
 
-  if let Err(e) = get_connection(&config.db_connection).await {
-    return Err(PythonError::IOError(e.message()));
-  }
-
-  let script_result = run_script(script_path, vec![db_url, prompt])?;
-  let result: Vec<&str> = script_result.split(&[' ', '\r', '\n']).collect();
-
-  // Should be either "Existing <id>" or "New <id>"
-  if result[0] == "Existing" {
-    // --- Parse the ID from the result ---
-    if let Ok(id) = result[1].parse::<i32>() {
-      return Ok(VectorSearchResult::Existing(id));
-    } else {
-      error!("Failed to parse the ID from the result: {}", result[1]);
-      return Err(PythonError::ResponseError("Received unparseable response.".to_string()));
+    if let Err(e) = get_connection(&config.db_connection).await {
+        return Err(PythonError::IOError(e.message()));
     }
-    // --- ---
-  } else if result[0] == "New" {
-    // --- Parse the ID from the result ---
-    if let Ok(id) = result[1].parse::<i32>() {
-      return Ok(VectorSearchResult::New(id));
+
+    let script_result = run_script(script_path, vec![prompt])?;
+    let result: Vec<&str> = script_result.split(&[' ', '\r', '\n']).collect();
+
+    // Should be either "Existing <id>" or "New <id>"
+    if result[0] == "Existing" {
+        // --- Parse the ID from the result ---
+        if let Ok(id) = result[1].parse::<i32>() {
+            return Ok(VectorSearchResult::Existing(id));
+        } else {
+            error!("Failed to parse the ID from the result: {}", result[1]);
+            return Err(PythonError::ResponseError("Received unparseable response.".to_string()));
+        }
+        // --- ---
+    } else if result[0] == "New" {
+        // --- Parse the ID from the result ---
+        if let Ok(id) = result[1].parse::<i32>() {
+            return Ok(VectorSearchResult::New(id));
+        } else {
+            error!("Failed to parse the ID from the result: {}", result[1]);
+            return Err(PythonError::ResponseError("Received unparseable response.".to_string()));
+        }
+        // --- ---
     } else {
-      error!("Failed to parse the ID from the result: {}", result[1]);
-      return Err(PythonError::ResponseError("Received unparseable response.".to_string()));
+        error!("Couldn't match response: {}", script_result);
+        return Err(PythonError::ResponseError("Couldn't match response.".to_string()));
     }
-    // --- ---
-  } else {
-    error!("Couldn't match response: {}", script_result);
-    return Err(PythonError::ResponseError("Couldn't match response.".to_string()));
-  }
 }
 
 /// Handles the prompt request.
@@ -143,11 +144,10 @@ pub async fn handle_vector_search(config: &Config, prompt: &String) -> Result<Ve
 /// This function logs the following:
 /// - The script result as `info` in the format `Script result: <script_result>`.
 pub fn handle_prompt_request(config: &Config, prompt: &String) -> Result<String> {
-  let script_path = config.rag_script.clone();
-  let llm_server = config.llm_connection.clone();
-  let script_result = run_script(script_path, vec![llm_server, prompt.clone()])?;
+    let script_path = config.rag_script.clone();
+    let script_result = run_script(script_path, vec![prompt.clone()])?;
 
-  info!("Script result: {}", script_result);
+    info!("Script result: {}", script_result);
 
-  Ok(script_result)
+    Ok(script_result)
 }
