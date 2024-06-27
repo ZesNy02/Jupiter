396f4307c18d236ee02d79b2fb10fa232f2a58e2
Tonka-de <rmcmaster05@gmail.com>
2024-06-27 19:53:18 +0200

    router done

--- website/src/Components/Navbar/Navbar.tsx
+++ website/src/Components/Navbar/Navbar.tsx
@@ -1,20 +1,79 @@
-import { AppBar, IconButton } from "@mui/material";
-import { FC } from "react";
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { FC, useState } from "react";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import "./Navbar.css";
+import { useLocation } from "react-router-dom";
 
 interface NavbarProps {}
 
+const routes = [
+  { route: "/", name: "Home" },
+  { route: "/about", name: "About" },
+  { route: "/documents", name: "Documents" },
+  { route: "/installation", name: "Installation" },
+];
+
 const Navbar: FC<NavbarProps> = ({}) => {
+  const [drawerState, setDrawerState] = useState(false);
+  const location = useLocation();
+
+  const openDrawer = () => {
+    setDrawerState(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerState(false);
+  };
+
+  const getSelectedPage = (path: string) => {
+    if (path === location.pathname) {
+      return { color: "primary.main" };
+    } else {
+      return { color: "primary.text" };
+    }
+  };
+
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="fixed" sx={{ padding: "0 0.5rem" }}>
         <div className="appbar-container">
-          <img className="logo" src="/logo.png" alt="logo" />
-          <IconButton>
+          <img className="logo" src="/logo_weiss.png" alt="logo" />
+          <IconButton
+            sx={{ aspectRatio: 1, height: "3rem" }}
+            onClick={openDrawer}
+          >
             <MenuRoundedIcon />
           </IconButton>
         </div>
       </AppBar>
+      <Drawer open={drawerState} onClose={closeDrawer} anchor="right">
+        <Box sx={{ width: "25rem" }}>
+          <List>
+            {routes.map((route, index) => (
+              <ListItem
+                key={index}
+                disablePadding
+                sx={getSelectedPage(route.route)}
+              >
+                <ListItemButton href={route.route}>
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText>{route.name}</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
     </>
   );
 };
